c7085dcac4eaa31290f77860ac0d76e7fe52148d
Zavid <zavidbohez@gmail.com>
2020-11-13 19:10:30 +0100

    add readFile

--- src/com/igkn/plugin/Main.java
+++ src/com/igkn/plugin/Main.java
@@ -3,14 +3,19 @@ package com.igkn.plugin;
 import com.goxr3plus.speech.translator.GoogleTranslate;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main extends JavaPlugin {
 
-    public static void main(String args[]) throws IOException {
+    static String fileLanguage="src/documents/languagesList.txt";
 
+    public static void main(String args[]) throws IOException {
+        HashMap<String,String> languages=readFile();
     }
 
     @Override
@@ -28,9 +33,28 @@ public class Main extends JavaPlugin {
                 new TranslateComand(this));
     }
 
-    public  HashMap<String,String> readFile(String nameFile){
+    public static HashMap<String,String> readFile(){
         HashMap<String,String> languages=new HashMap<String,String>();
 
+        try {
+            File myObj = new File(fileLanguage);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+
+                String[] parts = data.split(" ");
+                String languageName = parts[0];
+                String languageId = parts[1];
+
+                languages.put(languageName,languageId);
+
+                System.out.println("language: "+languageName+" id: "+languageId);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred in file lecture.");
+            e.printStackTrace();
+        }
 
         return languages;
     }
